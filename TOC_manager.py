a93aa052579a878fbf619e70408b954d64c60b39
胡可禧 <m10912069@yuntech.edu.tw>
2025-07-21 03:16:55 +0800

    [fix]修拉上櫃資料邏輯

--- TOC_manager.py
+++ TOC_manager.py
@@ -7,11 +7,11 @@ class TOC_manager:
     def __init__(self, trace_file_path='./raw_stock_data/trace.json', daily_data_dir='./raw_stock_data/daily/TOC'):
         self.url = 'https://www.tpex.org.tw/www/zh-tw/afterTrading/dailyQuotes'
         self.daily_data_dir = daily_data_dir
-        self.trace_data = []
+        self.trace_data = {}
 
     def save_trace_Specify_date(self, date, Issavetoday = False):
         # 清空之前的資料
-        self.trace_data = []
+        self.trace_data = {}
         
         # 檢查是否已經有今天的資料
         if os.path.exists(f'{self.daily_data_dir}/{date}.json'):
@@ -27,9 +27,11 @@ class TOC_manager:
         response = requests.post(self.url, data=payload)
         response.raise_for_status() # 非200會拋例外
         data_list = response.json()['tables'][0]['data']
+        get_date = response.json()['tables'][0]['date'].replace('/', '')
+        print(f"response date: {get_date}")
         for item in data_list:
-            self.trace_data.append({
-                "Date": date,
+            self.trace_data[item[0]] = {
+                "Date": get_date,
                 "Code" : item[0],
                 "Name" : item[1],
                 "TradeVolume" : item[8].replace(',',''),
@@ -41,8 +43,8 @@ class TOC_manager:
                 "Change" : item[3].replace('+','').replace(' ',''),   
                 "Transaction" : item[10].replace(',',''),
                 "AveragePrice" : item[7].replace(',','')                
-            })
-        with open(f'{self.daily_data_dir}/{date}.json', 'w', encoding='utf-8') as f:
+            }
+        with open(f'{self.daily_data_dir}/{get_date}.json', 'w', encoding='utf-8') as f:
             json.dump(self.trace_data, f, ensure_ascii=False, indent=0)
         if Issavetoday:
             with open(f'{self.daily_data_dir}/today.json', 'w', encoding='utf-8') as f:
@@ -64,6 +66,6 @@ class TOC_manager:
 
 if __name__ == "__main__":
     # 測試用
-    trace_manager = TraceManager()
+    trace_manager = TOC_manager()
     # trace_manager.save_trace_Specify_date('1130717')
     trace_manager.daily_trace()
